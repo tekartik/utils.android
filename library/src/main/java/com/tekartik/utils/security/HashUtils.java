9fec096e053111cd983aa8a12c774ae3f4b55b29
Alexandre Roux Tekartik <alex@tekartik.com>
2018-05-16 17:45:33 +0200

    feat: add sha1 and fix md5 hash implementation

--- library/src/main/java/com/tekartik/utils/security/HashUtils.java
+++ library/src/main/java/com/tekartik/utils/security/HashUtils.java
@@ -1,5 +1,7 @@
 package com.tekartik.utils.security;
 
+import com.tekartik.utils.hex.HexUtils;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -19,10 +21,26 @@ public class HashUtils {
             byte messageDigest[] = digest.digest();
 
             // Create Hex String
-            StringBuffer hexString = new StringBuffer();
-            for (int i=0; i<messageDigest.length; i++)
-                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
-            return hexString.toString();
+            return HexUtils.byteArrayToHexString(messageDigest).toLowerCase();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    static public String sha1(String s) {
+        if (s == null) {
+            return null;
+        }
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            return HexUtils.byteArrayToHexString(messageDigest).toLowerCase();
 
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
--- library/src/test/java/com/tekartik/utils/security/HashUtilsTest.java
+++ library/src/test/java/com/tekartik/utils/security/HashUtilsTest.java
@@ -11,10 +11,17 @@ import static org.junit.Assert.assertNull;
 
 public class HashUtilsTest {
     @Test
-    public void md5() throws Exception {
+    public void md5() {
         assertNull(HashUtils.md5(null));
-        assertEquals("d41d8cd98f0b24e980998ecf8427e", HashUtils.md5(""));
-        assertEquals("98f6bcd4621d373cade4e832627b4f6", HashUtils.md5("test"));
+        assertEquals("d41d8cd98f00b204e9800998ecf8427e", HashUtils.md5(""));
+        assertEquals("098f6bcd4621d373cade4e832627b4f6", HashUtils.md5("test"));
+    }
+
+    @Test
+    public void sha1() {
+        assertNull(HashUtils.sha1(null));
+        assertEquals("da39a3ee5e6b4b0d3255bfef95601890afd80709", HashUtils.sha1(""));
+        assertEquals("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3", HashUtils.sha1("test"));
     }
 
 }
