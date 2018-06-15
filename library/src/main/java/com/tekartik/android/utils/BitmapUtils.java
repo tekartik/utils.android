3bc563aca0d110d34020718a28e929f43f709681
Alexandre Roux Tekartik <alex@tekartik.com>
2018-06-15 12:14:23 +0200

    feat: more BitmapUtils

--- library/src/main/java/com/tekartik/android/utils/BitmapUtils.java
+++ library/src/main/java/com/tekartik/android/utils/BitmapUtils.java
@@ -2,6 +2,14 @@ package com.tekartik.android.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.support.media.ExifInterface;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 /**
  * Created by alex on 03/10/16.
@@ -31,5 +39,86 @@ public class BitmapUtils {
 
     static public Bitmap getResizedBitmap(int targetW, int targetH, String imagePath) {
         return decodeFromFile(targetW, targetH, imagePath);
+    }
+
+    static public String MIME_TYPE_PNG = "image/png";
+    static public String MIME_TYPE_JPEG = "image/jpeg";
+
+    // Square filling bitmap
+    public static Bitmap createSquaredBitmap(Bitmap srcBmp) {
+        int dim = Math.min(srcBmp.getWidth(), srcBmp.getHeight());
+        Bitmap dstBmp = Bitmap.createBitmap(dim, dim, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(dstBmp);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(srcBmp, (dim - srcBmp.getWidth()) / 2, (dim - srcBmp.getHeight()) / 2, null);
+
+        return dstBmp;
+    }
+
+    public static Bitmap resizeAndRecycleMinSize(Bitmap bm, int minSize) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+
+        float scale;
+        if (width > height) {
+            scale = (float) minSize / width;
+        } else {
+            scale = (float) minSize / height;
+        }
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bm, (int) (width * scale), (int) (height * scale), false);
+
+        bm.recycle();
+        return resizedBitmap;
+    }
+
+    public static String toBase64Png(Bitmap bm) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    public static String toBase64Jpeg(Bitmap bm) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    static public Integer getBestNeededRotation(String imagePath) {
+        try {
+            ExifInterface ei = new ExifInterface(imagePath);
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch (orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    return 90;
+
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    return 180;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    return 270;
+
+                case ExifInterface.ORIENTATION_NORMAL:
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
     }
 }
